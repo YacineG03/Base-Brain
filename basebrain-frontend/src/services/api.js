edef2990311a5e73273b5d210418251b324cf6f0
YacineG03 <132847649+YacineG03@users.noreply.github.com>
2025-03-20 15:48:56 +0000

    Configuration pour Render

--- basebrain-frontend/src/services/api.js
+++ basebrain-frontend/src/services/api.js
@@ -1,7 +1,11 @@
   import axios from "axios";
 
+  // const api = axios.create({
+  //   baseURL: "http://localhost:3001/api",
+  //   withCredentials: true,
+  // });
   const api = axios.create({
-    baseURL: "http://localhost:3001/api",
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
   });
 
